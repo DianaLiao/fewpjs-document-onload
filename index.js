d19a82ff452a2f418418c4e54faa9a5c06bd7f92
Diana Liao <dijali@gmail.com>
2021-04-06 19:09:50 -0400

    another mocha test failer that seems to work irl

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", coolness());
+
+console.log(
+  "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+);
+
+function coolness() {
+  let textToChange = document.getElementById("text")
+  textToChange.textContent = "This is really cool!"
+  console.log("The DOM has loaded")
+}
